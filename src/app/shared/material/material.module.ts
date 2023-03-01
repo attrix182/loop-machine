fa817ab52f5bbc13496228c4b3c515306addf886
Luciano S <luciano.sinisterra@lamercantil.com.ar>
2023-02-28 23:53:31 -0300

    test

---
+++ src/app/shared/material/material.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+
+const materialModules = [
+
+];
+
+
+@NgModule({
+  imports: [
+    CommonModule,
+
+  ],
+  exports: [ ]
+})
+export class MaterialModule { }
